6bebb22f02ea48a410502b3a8201f6f6f6f38ac6
Mimi Flynn <414934+mimiflynn@users.noreply.github.com>
2024-10-28 11:31:22 -0400

    Page head component refactor (#38)
    * update outdated
    
    * remove helmet and update templates to use head
    
    * update dependencies manually
    
    * update node build versions
    
    * lock sync
    
    * add mocks for tests
    
    * more tests and config
    
    * add test to pr build
    
    * override axios
    
    * update navigation
    
    * override webpack-dev-middleware
    
    * update axios
    
    * additoin of the page head component

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -6,6 +6,7 @@ import HeroContent from '../../content/hero.mdx';
 import Partnerships from '../../content/partnerships.mdx';
 
 import Layout from '../components/layout';
+import PageHead from '../components/page-head';
 
 const SiteIndex = ({ data, location }) => {
   return (
@@ -22,6 +23,10 @@ const SiteIndex = ({ data, location }) => {
 };
 
 export default SiteIndex;
+
+export const Head = ({ data }) => (
+  <PageHead title={data.site.siteMetadata.title} />
+);
 
 export const pageQuery = graphql`
   query {
